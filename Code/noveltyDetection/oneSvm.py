523262f4df9bd5e1799ee195ddb96c26ed91634e
fanshuoshuo <1065777269@qq.com>
2017-07-04 01:57:38 -0700

    add roc and f1

--- Code/noveltyDetection/oneSvm.py
+++ Code/noveltyDetection/oneSvm.py
@@ -8,8 +8,6 @@ import  sys
 sys.path.append("..")
 import  load_data
 import  numpy as np
-import  matplotlib.pyplot as plt
-import  matplotlib.font_manager
 #sklearn
 from sklearn import svm
 from sklearn.model_selection import train_test_split
@@ -21,40 +19,27 @@ from sklearn.metrics import roc_auc_score
 bank2data=load_data.load_2year()
 X=bank2data[0]
 y=bank2data[1]
-
+#split the data
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=0)
 
-clf=svm.OneClassSVM(nu=0.1,kernel="rbf",gamma=0.1)
-
+clf=svm.OneClassSVM(nu=0.1,kernel="rbf")
+#fit the data
 clf.fit(X_train)
 y_pred_train=clf.predict(X_train)
-
-
-#print(y_pred_train)
 y_pred_test=clf.predict(X_test)
-#print(y_pred_test)
-
-index=0
-for row in y_pred_train:
-    if(row==1):
-        y_pred_train[index]=0
-    else:
-        y_pred_train[index]=1
-    index+=1
-
-index=0
-for row in y_pred_test:
-    if(row==1):
-        y_pred_test[index]=0
-    else:
-        y_pred_test[index]=1
-    index+=1
-
+#replace(1,-1) to (0,-1)
+y_pred_train=np.where(y_pred_train>0,0,1)
+y_pred_test =np.where(y_pred_test >0,0,1)
+#show the performance of the OneClassSVM
 print("train data")
 print(classification_report(y_train,y_pred_train))
+print("f1_score is ",f1_score(y_train,y_pred_train))
+print("roc_auc is ",roc_auc_score(y_train,y_pred_train))
 print()
 print("test data")
 print(classification_report(y_test,y_pred_test))
+print("f1_score is ",f1_score(y_test,y_pred_test))
+print("roc_auc is ",roc_auc_score(y_test,y_pred_test))
 print()
 
 
